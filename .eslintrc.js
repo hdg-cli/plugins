ba27bbe1138a402a08d1bd395a46a003ea2e47ca
hdg <huangdaigao@qumaipiao.net>
2022-03-01 17:52:53 +0800

    eslint配置完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,7 @@ module.exports = {
     env: {
         node: true
     },
-    extends: [
-        'plugin:vue/vue3-essential',
-        'eslint:recommended',
-        '@vue/typescript/recommended',
-        'prettier',
-        '@vue/prettier',
-        '@vue/prettier/@typescript-eslint'
-    ],
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended', 'prettier', '@vue/prettier'],
     plugins: ['prettier'],
     parserOptions: {
         ecmaVersion: 2017
@@ -18,7 +11,45 @@ module.exports = {
     rules: {
         'prettier/prettier': 'error',
         'arrow-body-style': 'off',
-        'prefer-arrow-callback': 'off'
+        'prefer-arrow-callback': 'off',
+        'no-console': 0,
+        'no-tabs': 'off',
+        'generator-star-spacing': 'off',
+        'semi': ['error', 'never'],
+        'indent': ['error', 4],
+        'linebreak-style': [0, 'error', 'window'],
+        'quotes': ['error', 'single'],
+        'brace-style': ['error', '1tbs'],
+        'no-new': ['off'],
+        'camelcase': [
+            'warn',
+            {
+                'properties': 'never'
+            }
+        ],
+        'space-before-function-paren': [
+            'error',
+            {
+                'anonymous': 'always',
+                'named': 'never',
+                'asyncArrow': 'never'
+            }
+        ],
+        'spaced-comment': [
+            'warn',
+            'always',
+            {
+                'line': {
+                    'markers': ['/'],
+                    'exceptions': ['-', '+']
+                },
+                'block': {
+                    'markers': ['!', '*'],
+                    'exceptions': ['*'],
+                    'blanced': true
+                }
+            }
+        ]
     },
     overrides: [
         {
